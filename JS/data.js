8aa359e4705974474d4a45f4bdc0180156d50d73
Ovidiu P <peptenaru.ovidiu@yahoo.com>
2022-02-08 13:41:28 +0200

    some styling changes

--- JS/data.js
+++ JS/data.js
@@ -17,3 +17,29 @@ window.addEventListener("scroll", ()=>{
         getBackButton.style.visibility = "hidden";
     }
 })
+
+//counter
+
+
+const counters = document.querySelectorAll('.counter');
+
+
+counters.forEach(counter =>{
+    counter.innerText = '0'
+
+    const updateCounter = ()=>{
+            const target = +counter.getAttribute('data-target')
+    const c = parseInt(counter.innerHTML) 
+
+    const increment = target / 2000;
+
+     if(c < target){
+         counter.innerHTML = `${Math.ceil(c + increment)}`
+         setTimeout(updateCounter ,10)
+     }else{
+         counter.innerText = target
+     }
+    }
+ updateCounter()
+
+})
